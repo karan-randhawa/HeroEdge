d9df9ddd42fe32ed03bb2a8af3d53073b50bc130
Karan Randhawa <karanrandhawa_1@live.com>
2017-04-24 04:30:21 -0400

    Initial work/still needs improvements on Cortana integration

---
+++ MarvelFacade.cs
@@ -0,0 +1,137 @@
+﻿using HeroEdge.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Security.Cryptography;
+using Windows.Security.Cryptography.Core;
+using Windows.Storage.Streams;
+
+namespace HeroEdge
+{
+    public class MarvelFacade
+    {
+        /*
+         * Public Key:  f2394b74cc9cc2140d81d97425d3525a
+         * Private Key: 9d48644ec5deb09c500671d9fcfa4733999c2cf3
+         */
+        private const string PrivateKey = "9d48644ec5deb09c500671d9fcfa4733999c2cf3";
+        private const string PublicKey = "f2394b74cc9cc2140d81d97425d3525a";
+        private const string ImageNotAvailablePath = "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available";
+        private const int MaxCharacters = 1500;
+
+        public static async Task PopulateMarvelCharactersAsync(ObservableCollection<Character> marvelCharacters)
+        {
+            try
+            {
+                var characterDataWrapper = await GetCharacterDataWrapperAsync();
+                var characters = characterDataWrapper.data.results;
+                foreach (var character in characters)
+                {
+                    // Filter characters that are missing thumbnail 
+                    if (character.thumbnail != null && character.thumbnail.path != "" 
+                                 && character.thumbnail.path != ImageNotAvailablePath)
+                    {
+                        character.thumbnail.small = String.Format("{0}/standard_small.{1}",
+                            character.thumbnail.path,
+                            character.thumbnail.extension);
+                        character.thumbnail.large = String.Format("{0}/portrait_xlarge.{1}",
+                            character.thumbnail.path,
+                            character.thumbnail.extension);
+                        marvelCharacters.Add(character);
+                    }
+                }
+            }
+            catch(Exception) { return; }
+        }
+
+        public static async Task PopulateMarvelComicsAsync(int characterId, ObservableCollection<ComicBook> marvelComics)
+        {
+            try
+            {
+                var comicDataWrapper = await GetComicDataWrapperAsync(characterId);
+                var comics = comicDataWrapper.data.results;
+                foreach (var comic in comics)
+                {
+                    // Filter characters that are missing thumbnail 
+                    if (comic.thumbnail != null && comic.thumbnail.path != ""
+                                 && comic.thumbnail.path != ImageNotAvailablePath)
+                    {
+                        comic.thumbnail.small = String.Format("{0}/portrait_medium.{1}",
+                            comic.thumbnail.path,
+                            comic.thumbnail.extension);
+                        comic.thumbnail.large = String.Format("{0}/portrait_xlarge.{1}",
+                            comic.thumbnail.path,
+                            comic.thumbnail.extension);
+                        marvelComics.Add(comic);
+                    }
+                }
+            }
+            catch (Exception) { return; }
+        }
+
+        private static async Task<CharacterDataWrapper> GetCharacterDataWrapperAsync()
+        {
+            // Assemble the URL
+            Random random = new Random();
+            var offset = random.Next(MaxCharacters);
+
+            var apiUrl = String.Format("http://gateway.marvel.com:80/v1/public/characters?limit=30&offset={0}", offset);
+            var jsonMessage = await CallMarvelAPIAsync(apiUrl);
+
+            // Response -> string / json -> deserialize
+            var serializer = new DataContractJsonSerializer(typeof(CharacterDataWrapper));
+            var ms = new MemoryStream(Encoding.UTF8.GetBytes(jsonMessage));
+            var result = (CharacterDataWrapper)serializer.ReadObject(ms);
+            return result;
+        }
+
+        private static async Task<ComicDataWrapper> GetComicDataWrapperAsync(int characterId)
+        {
+            string apiUrl = String.Format("https://gateway.marvel.com:443/v1/public/comics?characters={0}&limit=10", characterId);
+            var jsonMessage = await CallMarvelAPIAsync(apiUrl);
+
+            // Response -> string / json -> deserialize
+            var serializer = new DataContractJsonSerializer(typeof(ComicDataWrapper));
+            var ms = new MemoryStream(Encoding.UTF8.GetBytes(jsonMessage));
+            var result = (ComicDataWrapper)serializer.ReadObject(ms);
+            return result;
+        }
+
+        private static async Task<string> CallMarvelAPIAsync(string apiUrl)
+        {
+            // Get the MD5 Hash
+            var timeStamp = DateTime.Now.Ticks.ToString();
+            var hash = CreateHash(timeStamp);
+
+            string completeUrl = String.Format("{0}&apikey={1}&ts={2}&hash={3}", apiUrl, PublicKey, timeStamp, hash);
+
+            // Call out to Marvel
+            HttpClient http = new HttpClient();
+            var response = await http.GetAsync(completeUrl);
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        private static string CreateHash(string timeStamp)
+        {
+            var toBeHashed = timeStamp + PrivateKey + PublicKey;
+            var hashedMessage = ComputeMD5(toBeHashed);
+            return hashedMessage;
+        }
+        // From:
+        // http://stackoverflow.com/questions/8299142/how-to-generate-md5-hash-code-for-my-winrt-app-using-c
+        private static string ComputeMD5(string str)
+        {
+            var alg = HashAlgorithmProvider.OpenAlgorithm(HashAlgorithmNames.Md5);
+            IBuffer buff = CryptographicBuffer.ConvertStringToBinary(str, BinaryStringEncoding.Utf8);
+            var hashed = alg.HashData(buff);
+            var res = CryptographicBuffer.EncodeToHexString(hashed);
+            return res;
+        }
+    }
+}
